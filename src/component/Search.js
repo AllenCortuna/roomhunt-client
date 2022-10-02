08f3bb5d73ba92d3e33ad61863471dcc8546bb03
zan <olsencortuna@gmail.com>
2022-10-02 15:15:07 +0800

    searchbar changes

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,11 +1,51 @@
-import React from 'react'
-import SearchBar from './utility/SearchBar'
-const Search = () => {
-  return (
-    <div className=''>
-    <SearchBar/>
-    </div>
-  )
-}
+import React from "react";
+import { BsFilter } from "react-icons/bs";
+import { MdOutlineHouse } from "react-icons/md";
+import { TbBeach } from "react-icons/tb";
+import { RiHotelLine } from "react-icons/ri";
+import SearchBar from "./utility/SearchBar";
 
-export default Search
+
+const Search = () => {
+  const iconClass =
+    "text-[1.5rem] inline text-gray-400 ";
+  const type = [
+    {
+      name: "Rentals",
+      icon: <MdOutlineHouse className={`${iconClass}`} />,
+    },
+    {
+      name: "Hotel",
+      icon: <RiHotelLine className={`${iconClass}`} />,
+    },
+    { name: "Resort", icon: <TbBeach className={`${iconClass}`} /> },
+  ];
+  return (
+    <div className="grid grid-cols-1 border border-gray-200 bg-white shadow-md rounded-lg p-4 maw-w-[45rem] min-w-[22rem]">
+      {/* <SearchBar/> */}
+      <SearchBar/>
+      {/* filter */}
+      <span className="pt-5">
+        <BsFilter className="text-[1.5rem] text-gray-600 inline" />
+        <p className="ml-3 inline text-sm font-semibold text-gray-500">
+          Filters
+        </p>
+        <hr className="text-gray-400 w-full mt-2" />
+      </span>
+
+    {/* Property type checkbox */}
+    <span className="grid grid-cols-3 gap-2">
+    <h4 className="mt-3 ml-1 text-[.8rem] col-span-3 text-gray-500 font-semibold">Property Type</h4>
+    {type.map((a)=>(
+      <span className="flex flex-wrap justify-center text-center flex-row border border-gray-400 p-2 rounded-lg ">
+      {a.icon}
+      <p className="ml-2 mt-1 inline text-xs text-gray-500">{a.name}</p>
+</span>
+    ))}
+    </span>
+
+    </div>
+  );
+};
+
+export default Search;
--- src/component/utility/SearchBar.js
+++ src/component/utility/SearchBar.js
@@ -4,22 +4,19 @@ import { BsSearch } from "react-icons/bs";
 const SearchBar = () => {
   return (
     <>
-      <div className="flex flex-wrap justify-center gap-1 h-min p-2 mx-auto w-80 rounded-full shadow-sm border max-w-md bg-white ">
+      {/* <hr className="w-screen" /> */}
+    <div className="flex flex-wrap justify-center gap-1 h-min p-2 mx-auto max-w-[30rem] min-w-[20rem] rounded-full shadow-md border max-w-md bg-white ">
         <span>
           <input
             type="text"
-            placeholder="search"
-            className="w-60 h-9 text-xs font-normal p-2 text-gray-500 bg-none rounded-lg rounded-br-none rounded-tr-none text-center border col-span-2 focus:font-semibold focus:border-gray-400 focus:outline-none focus:border-none focus:text-gray-400 "
+            placeholder="search (eg.location,name)"
+            className="w-60 h-9 text-xs font-normal p-2 text-gray-600 bg-none rounded-lg text-center  col-span-2 focus:outline-none focus:border-none focus:text-gray-500 "
           />
         </span>
-        <span className="bg-gray-200 rounded-xl rounded-tl-none rounded-bl-none border-2 shadow-sm h-9  text-center w-8">
-<BsSearch
-            className="text-center w-6 my-auto text-gray-500 center mt-2"
-
-          />
+        <span className="bg-gray-200 rounded-xl border-2 shadow-sm h-8 w-8 flex flex-wrap justify-center align-center">
+          <BsSearch className="w-8 my-auto text-gray-500 center " />
         </span>
       </div>
-      {/* <hr className="w-screen" /> */}
     </>
   )
 }
