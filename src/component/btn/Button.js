9b0df4a0760fb729719169031e27b3109759e96e
zan <olsencortuna@gmail.com>
2022-10-09 20:04:21 +0800

    Otp form ok

--- src/component/btn/Button.js
+++ src/component/btn/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Button = ({ text, color, onClick, txtColor }) => {
+const Button = ({ text, color, onClick, txtClr }) => {
   return (
     <button
-      className={`transition-all duration-300 ease-linear p-4 py-3 bg-${color} text-xs md:text-sm font-[500] shadow-lg rounded-md mt-2 text-gray-600 mr-2 ml-auto min-w-[5rem] hover:shadow hover:font-[800]`}
+      className={`transition-all duration-300 ease-linear p-4 py-3 ${color} text-xs md:text-sm font-[500] shadow-md rounded-md text-gray-600 min-w-[5rem] hover:shadow md:text-sm ${txtClr} inline`}
       onClick={onClick}
     >
       {text}
--- src/register/accommodator/Acc3.js
+++ src/register/accommodator/Acc3.js
@@ -1,17 +1,57 @@
 import React from "react";
-
+import { submit } from "../../style/register";
+import Button from "../../component/btn/Button";
 const Acc3 = () => {
-  const [otp, setotp] = React.useState("");
-  const handleChange = (e) => {
-    setotp(e.target.value);
-    console.log(otp);
+  const [otp, setOtp] = React.useState(new Array(4).fill(""));
+
+  const handleChange = (element, index) => {
+    if (isNaN(element.value)) return false;
+
+    setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]); //Focus next input
+
+    if (element.nextSibling) {
+      element.nextSibling.focus();
+    }
   };
 
   return (
     <>
+      <div className="flex flex-wrap justify-center ">
+<p className="text-gray-500 text-sm mt-5 mb-4">Please enter the OTP sent to your email</p>
+
+        <div className="">
+          {otp.map((data, index) => {
+            return (
+              <input
+                className="text-center h-10 w-8 border-2 border-gray-400 rounded-md space-mono text-gray-500 text-xl m-1 focus:outline-none focus:border-gray-500"
+                type="text"
+                name="otp"
+                maxLength="1"
+                key={index}
+                value={data}
+                onChange={(e) => handleChange(e.target, index)}
+                onFocus={(e) => e.target.select()}
+              />
+            );
+          })}
+        </div>
+        <span className= "flex flex-wrap justify-between w-[13rem] mt-5">
+          <Button
+            onClick={() => setOtp([...otp.map(() => "")])}
+            text={"Clear"}
+            color={"bg-gray-300"}
+            txtClr={"text-gray-400"}
+          />
+          <Button
+            onClick={() => alert("Entered OTP is " + otp.join(""))}
+            text={"Verify"}
+            color={"bg-secondary"}
+          />
+        </span>
+      </div>
+              
     </>
   );
 };
 
 export default Acc3;
-
