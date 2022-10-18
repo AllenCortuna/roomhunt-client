72a0388767427b9c24530401326b904be7a150ff
zan <olsencortuna@gmail.com>
2022-10-18 12:55:00 +0800

    home layout chnages

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,43 +1,11 @@
+import logo from "./img/roomhunt_logo.png";
+
 import React from "react";
-import resort from "./img/resort.svg";
-import hotel from "./img/hotel.svg";
-import house from "./img/house.svg";
-import Search from "./Search";
-import {
-  container,
-  contentContainer,
-  content,
-  contentImg,
-  contentName,
-  contentDesc,
-} from "./style/home.js";
-
-// NOTE:all className are in ./style/home.js dir
+import  Search  from "./Search";
 const Home = () => {
-  const contents = [
-    {
-      name: "Rental Homes",
-      img: house,
-      desc: "Search for the boarding house or rental homes near your school or work.",
-    },
-    { name: "Hotel", img: hotel, desc: "Avail the room that fits your needs." },
-    { name: "Resort", img: resort, desc: "Find the best resort avialable." },
-  ];
-
   return (
-    <div className={container}>
-      {/* search component */}
-      <Search />
-      {/* contents */}
-      <div className={contentContainer}>
-        {contents.map((cont) => (
-          <div className={content} key={cont.name}>
-            <img className={contentImg} src={cont.img} alt="content" />
-            <h1 className={contentName}>{cont.name}</h1>
-            <p className={contentDesc}>{cont.desc}</p>
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-wrap justify-center m-0 w-full p-5 mt-5">
+    <Search/>
     </div>
   );
 };
