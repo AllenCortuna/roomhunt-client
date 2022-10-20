88f57045935023465a814f5fc7ffdd1ebb8f8aef
zan <olsencortuna@gmail.com>
2022-10-20 08:58:20 +0800

    refactor: lautout button

--- src/component/Logout.js
+++ src/component/Logout.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRegisterState } from "../state/register";
 import Button from "./btn/Button";
@@ -8,16 +7,16 @@ const Logout = () => {
 
   const logOut = useRegisterState((state) => state.logOut);
   return (
-    <div className="grid grid-cols-2 place-items-center p-4 mt-40 gap-5 min-w-[18rem] max-w-[24rem]">
+    <div className="grid grid-cols-2 place-items-center p-4 mt-40 md:mt-28 gap-5 min-w-[18rem] max-w-[24rem] mx-auto">
       <span className="col-span-2 text-center">
         <h1 className="font-bold uppercase text-cyan-800 text-xl drop-shadow-md ">
           logout
         </h1>
-        <p className="text-sm text-gray-400 rale">Logout and clear session ?</p>
+        <p className="text-sm text-gray-400 rale">Logout and clear Account info ?</p>
       </span>
       <Button
         color={
-          "bg-none border-2 border-gray-500 text-gray-400 shadow-none"
+          "bg-none border-2 border-gray-500 text-gray-400 shadow-none mt-5 mr-0 ml-auto"
         }
         loading={false}
         text={"cancel"}
@@ -28,10 +27,10 @@ const Logout = () => {
 
       <Button
         color={
-          "border-2 bg-error shadow-md text-gray-100 "
+          "border-2 border-error bg-error shad mt-5 ml-0 mr-auto"
         }
         loading={false}
-        text={"Logout"}
+        text={<p className="text-white">Logout</p>}
         onClick={() => {
           logOut(navigate);
         }}
