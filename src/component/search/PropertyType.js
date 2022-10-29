416918f51134afa90e47b754ce0dfed5cb344764
zan <olsencortuna@gmail.com>
2022-10-29 17:21:37 +0800

    minor changes

--- src/component/search/PropertyType.js
+++ src/component/search/PropertyType.js
@@ -19,7 +19,7 @@ const PropertyType = ({ handleCat,query }) => {
   return (
     // <span className="grid rounded-tl-md rounded-tr-md p-3 pb-4 bg-gray-100 shadow-sm mb-2">
     <span className="grid">
-      <span className="grid grid-cols-3 gap-2 mt-2">
+      <span className="grid grid-cols-3 gap-3 mt-2">
         {type.map((a) => (
           <span
             className={`group flex flex-wrap justify-center items-center text-center flex-row p-2 rounded-lg  transition-all duration-300 ease-linear shad ${query.category===a.name ? "bg-yellow " : "bg-smoke"}`}
@@ -27,7 +27,7 @@ const PropertyType = ({ handleCat,query }) => {
             onClick={()=>handleCat(a.name)}
           >
             {a.icon}
-            <p className={`truncate group-hover:text-gray-500 group-hover:font-semibold ml-2 inline text-[.8rem] group-hover:drop-shadow-md ${query.category === a.name ? "text-gray-500 font-semibold" : "text-gray-400"}`}>
+            <p className={`truncate group-hover:text-gray-500 group-hover:font-semibold ml-1 inline text-[.8rem] group-hover:drop-shadow-md ${query.category === a.name ? "text-gray-500 font-semibold" : "text-gray-400"}`}>
               {a.name}
             </p>
           </span>
