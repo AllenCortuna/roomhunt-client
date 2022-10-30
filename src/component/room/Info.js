a6e9db1e13ee066660f19f07fd03b7a9b8a0ba11
zan <olsencortuna@gmail.com>
2022-10-30 12:16:43 +0800

    chore: ui updates

---
+++ src/component/room/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Info = ({room}) => {
+  return (
+    <span>
+
+
+    </span>
+  )
+}
+
+export default Info
--- src/component/room/RoomInfo.js
+++ src/component/room/RoomInfo.js
@@ -4,6 +4,9 @@ import Loading from '../utility/Loading'
 import React, { useEffect } from 'react'
 import { roomStore } from "../../state/room";
 import { useParams } from 'react-router-dom';
+import { MdLocationOn } from "react-icons/md";
+import { RiBuildingFill } from "react-icons/ri";
+import Info from './Info';
 
 const RoomInfo = () => {
 
@@ -22,14 +25,30 @@ const RoomInfo = () => {
   return (
     <>
       {loading ? waiting :
-        <span className='grid md:grid-cols-2 gap-5'>
-          <span className="rounded-lg ">
-            <img
-              src={room.image}
-              alt="room"
-              className='w-full h-[20rem] mb-5'
-            />
+        <span className='grid md:grid-cols-3 gap-5 w-[21rem] md:w-[42rem] mt-14 mb-14 md:mt-20 rounded-lg bg-white shadow-md mx-auto border'>
+          <img
+            src={room.image}
+            alt="room"
+            className='object-cover w-full h-[24rem] rounded-tl-lg rounded-tr-lg md:rounded-bl-lg md:rounded-tr-none'
+          />
+          <span className="md:col-span-2">
+            {/* room owner */}
+            <span className='grid p-2 bg-zinc-200  w-auto h-auto rounded-tr-xl rounded-br-xl mr-4 my-4'>
+              <h4 className="drop-shadow-sm text-cyan-800 font-bold">
+                <RiBuildingFill className='inline mr-1 mb-1' />
+                {room.ownerName}</h4>
+              <h4 className="text-zinc-500 text-xs">
+                <MdLocationOn className='inline mb-1' />
+                {room.location}</h4>
+            </span>
+            {/* room info */}
+            <span className='grid md:grid-cols-2'>
+              <Info />
+              {/* room review */}
+              <span>
 
+              </span>
+            </span>
           </span>
         </span>}
     </>
