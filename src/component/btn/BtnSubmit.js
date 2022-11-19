5adbcc7cab00030f7125ba657f952654a0f10e26
zan <olsencortuna@gmail.com>
2022-11-20 06:58:44 +0800

    chore: ui btn loading

--- src/component/btn/BtnSubmit.js
+++ src/component/btn/BtnSubmit.js
@@ -4,13 +4,16 @@ const BtnSubmit = ({ loading, text, loadingTxt }) => {
   return (
     <button
       type="submit"
-      className={`transition-all duration-200 ease-linear border-2 p-4 py-3 text-xs md:text-sm font-[500] shad rounded-md mt-5 text-gray-600 mr-0 ml-auto ${
+      className={`transition-all duration-200 ease-linear border-2 p-4 py-3 text-xs md:text-sm font-[500] shad rounded-md mt-5 text-gray-600 mr-0 ml-auto flex flex-row gap-1 ${
         loading
           ? "border-gray-500 text-gray-400 bg-none"
           : "bg-secondary border-secondary"
       } `}
       disabled={loading}
     >
+      {loading && (
+        <span className="border-[.1rem] rounded-[.2rem] animate-spin bg-none border-zinc-500 w-4 h-4 my-auto"></span>
+      )}
       {loading ? loadingTxt : text}
     </button>
   );
