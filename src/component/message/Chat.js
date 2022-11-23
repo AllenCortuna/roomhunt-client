10ed02e5bef9a515dd011f552fe4b809ccbf8cb2
zan <olsencortuna@gmail.com>
2022-11-23 13:59:25 +0800

    stablemessage

--- src/component/message/Chat.js
+++ src/component/message/Chat.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Moment from "react-moment";
+import { useNavigate } from "react-router-dom";
+import { messageStore } from "../../state/message";
 
 const Chat = ({ chat }) => {
+  const loading = messageStore((state) => state.loading);
+  const deleteMessage = messageStore((state) => state.deleteMessage);
+ const navigate= useNavigate() 
+
   return (
-    <span className="flex gap-3 flex-row content-start justify-start sm:w-[16rem] md:max-w-[20rem] rounded bg-white rounded-lg p-4 whitespace-wrap shadow">
+    <span key={chat._id} className="group flex gap-4 flex-row content-start justify-start w-[20rem] rounded bg-white rounded-lg p-3 whitespace-wrap shadow">
       <span className="flex flex-cols justify-start content-center mt-[.5]">
-        <span className="rounded-full h-8 w-8 bg-zinc-600 text-white text-2xl font-bold text-center flex justify-center content-center uppercase ">
+        <span className="rounded-full h-8 w-8 bg-zinc-600 text-white text-2xl font-bold text-center flex justify-center content-center uppercase">
           {chat.sender.charAt(0)}
         </span>
       </span>
-      <span className="grid">
+   
+    {/* content */}
+      <span className="flex flex-col w-full gap-1">
         <span className="mb-1">
           <h4 className="text-xs font-[500] text-zinc-500">{chat.sender}</h4>
           <h4 className="text-[.6rem] rale font-[500] text-zinc-400">
@@ -17,8 +25,29 @@ const Chat = ({ chat }) => {
           </h4>
         </span>
         <h4 className="text-xs text-zinc-500 rale">{chat.message}</h4>
+        <span className="ease-linear duration-300 transition-all grid-cols-2 scale-0 grid  group-hover:scale-100 gap-3  h-0 group-hover:h-full pt-1 mr-0 ml-auto ">
+          <button
+            type="button"
+            onClick={()=> 
+            navigate(
+              `/messageSend?&sender=${chat.reciever}&senderId=${chat.recieverId}&reciever=${chat.sender}&recieverId=${chat.senderId}`
+            )}
+            className="text-[.6rem] font-semibold p-1 rounded-[.3rem] border-[0.085rem] border-zinc-400 bg-none text-zinc-400 shadow-sm"
+            disabled={loading}
+          >
+            reply
+          </button>
+
+          <button
+            type="button"
+            onClick={() => deleteMessage(chat._id)}
+            className="text-[.6rem] font-semibold p-1 rounded-[.3rem] border border-rose-600 bg-rose-600 text-white shadow-sm truncate"
+            disabled={loading}
+          >
+            {loading ? "processing" : "delete"}
+          </button>
+        </span>
       </span>
-      {/* todo: delete reply */}
     </span>
   );
 };
--- src/state/message.js
+++ src/state/message.js
@@ -17,7 +17,7 @@ api.interceptors.request.use((req) => {
 });
 
 export const messageStore = create((set) => ({
-  messages: [],
+  message: [],
   err: null,
   loading: false,
 
@@ -42,26 +42,42 @@ export const messageStore = create((set) => ({
       set({ message: result.data.result });
     } catch (err) {
       set({ err: err.response.data.message });
+      alert(err.response.data.message);
     }
     set({ loading: false });
   },
 
-  sendMessage: async (data,navigate) => {
+  sendMessage: async (data, navigate) => {
     set({ loading: true });
     set({ acc: null });
     try {
-      const result = await api.post("message/send", data );
-      set((state) => ({ messages: [...state.messages, result.data] }));
+      const result = await api.post("message/send", data);
+      set((state) => ({ message: [...state.message, result.data] }));
       navigate(-1);
-      alert("Message Sent!")
+      alert("Message Sent!");
     } catch (err) {
       set({ err: err.response.data.message });
-      console.log(err.response.data.message);
+      alert(err.response.data.message);
     }
     set({ loading: false });
   },
 
   setErr: (err) => {
     set({ err: err });
+  },
+
+  deleteMessage: async (id) => {
+    set({ loading: true });
+    set({ err: null });
+    try {
+      await api.delete(`/message/delete/${id}`);
+      set((state) => ({
+        message: state.message.filter((a) => a._id !== id),
+      }));
+    } catch (err) {
+      // set({ err: err.response.data.message });
+      alert(err.response.data.message);
+    }
+    set({ loading: false });
   },
 }));
