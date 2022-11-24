01d698468b9f7ffdbafe3c76b3028ae036b83fe8
zan <olsencortuna@gmail.com>
2022-11-24 19:07:45 +0800

    feat: room ui upload

--- src/component/upload/Room.js
+++ src/component/upload/Room.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { RiShieldKeyholeFill } from "react-icons/ri";
-import { BsCalendarWeek } from "react-icons/bs";
+import { BsFillCalendarWeekFill, BsFillGridFill } from "react-icons/bs";
+import { ImPriceTag } from "react-icons/im";
+import { MdLocationOn } from "react-icons/md";
+import { IoIosBed } from "react-icons/io";
 import Moment from "react-moment";
 import { roomStore } from "../../state/room";
 
 const Room = ({ room, setid }) => {
   const deleteRoom = roomStore((state) => state.deleteRoom);
-  const txt = "text-gray-500 font-[500] text-[.64rem] space-mono truncate";
-  const highlight = "text-gray-500 font-semibold  text-[.64rem] space-mono";
+  const icon = "inline";
+
   const loading = roomStore((state) => state.loading);
   return (
     <span
@@ -21,38 +23,39 @@ const Room = ({ room, setid }) => {
         className="rounded-tr-md rounded-tl-md object-cover w-full h-28"
       />
       <span className="p-2 py-0 grid">
-        <h1 className="text-cyan-800 font-semibold text-xs capitalize ">
-          <RiShieldKeyholeFill className="mr-1 inline" />
-          {room.name}
-        </h1>
-        <h1 className={highlight}>
-          <span className="rale font-normal text-gray-400">Price:</span> &#8369;
-          {room.price}
-        </h1>
-        <h1 className={highlight}>
-          <span className="rale font-normal text-gray-400">Bed count:</span>{" "}
-          {room.bed}
-        </h1>
 
-        <h1 className={txt}>
-          <BsCalendarWeek className="inline mr-1" />
-          <span className="rale font-normal text-gray-400">CheckIn:</span>{" "}
-          {room.checkInDate ? (
-            <Moment date={room.checkInDate} format="MMM-DD-YYYY" />
-          ) : (
-            "null"
-          )}
-        </h1>
+        <Content
+          icon={<ImPriceTag className={icon} />}
+          text={"price"}
+          value={<span> &#8369;{room.price}</span>}
+        />
+        <Content
+          icon={<BsFillGridFill className={icon} />}
+          text={"category"}
+          value={room.category}
+        />
+        <Content
+          icon={<IoIosBed className={icon} />}
+          text={"no. of bed"}
+          value={room.bed}
+        />
+        <Content
+          icon={<BsFillCalendarWeekFill className={icon} />}
+          text={"unavailableUntil"}
+          value={
+            room.unavailableUntil ? (
+              <Moment date={room.unavailableUntil} format="MMM-DD-YYYY" />
+            ) : (
+              "None"
+            )
+          }
+        />
+    
+        <Place
+          icon={<MdLocationOn className={icon} />}
+          value={room.location}
+        />
 
-        <h1 className={txt}>
-          <BsCalendarWeek className="inline mr-1" />
-          <span className="rale font-normal text-gray-400">CheckOut:</span>{" "}
-          {room.checkOutDate ? (
-            <Moment date={room.checkOutDate} format="MMM-DD-YYYY" />
-          ) : (
-            "null"
-          )}
-        </h1>
         {/* btn */}
         <span className="ease-linear duration-300 transition-all grid-cols-2 scale-0 grid  group-hover:scale-100 gap-3 w-full h-0 group-hover:h-full pt-1">
           <button
@@ -78,6 +81,24 @@ const Room = ({ room, setid }) => {
         </span>
       </span>
     </span>
+  );
+};
+const Content = ({ icon, text, value }) => {
+  return (
+    <h1 className={"text-gray-600 font-[500]  text-[.64rem] capitalize"}>
+      <span className="mr-1 inline">{icon}</span>
+      <span className="mr-1 rale font-normal text-gray-500 capitalize">{text}</span>
+      {value}
+    </h1>
+  );
+};
+
+const Place = ({ icon, value }) => {
+  return (
+    <h1 className={"text-gray-500 font-semibold  text-[.64rem] truncate "}>
+      <span className="mr-1 inline">{icon}</span>
+      <span className="mr-1 rale font-normal text-gray-500 capitalize">{value}</span>
+    </h1>
   );
 };
 
